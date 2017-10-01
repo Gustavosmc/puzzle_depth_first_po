cb507ce19389432bad09a43f1e1fb6df910d0f5b
Gustavosmc <luizgustavosmc@gmail.com>
2017-10-01 14:03:38 -0300

    ...

--- busca_profundidade.py
+++ busca_profundidade.py
@@ -1,7 +1,9 @@
 import copy
 
+
 visitados = []
 caminho = []
+cont_id = 1
 
 
 class Estado(object):
@@ -15,27 +17,34 @@ class Estado(object):
         self.tam_estado = (self.tam_x * self.tam_y) - 1
         self.pos_coringa = self.estado.find(self.coringa)
         self.pai = None
-        self.limite = 100
+        self.profundidade = 1
+        self.id = cont_id
+
 
     def proximo(self):
+        global cont_id
+        cont_id += 1
+
         self.expande()
         caminho.append(self)
-        for filho in self.filhos:
-            if filho != self.pai:
-                if not filho.visitado:
-                    filho.visitado = True
-                    return filho
 
-        return self.pai
+        if self.profundidade == 10:
+            return self.pai
+
+        for filho in self.filhos:
+            if filho != self:
+                if filho not in visitados:
+                    visitados.append(filho)
+                    return filho
+        ret = self.pai
+        del self
+        return ret
 
     def expande(self):
         self.move_abaixo()
         self.move_direita()
         self.move_acima()
         self.move_esquerda()
-
-    def copiar(self):
-        return copy.copy(self)
 
     def move_abaixo(self):
         obj_estado = None
@@ -48,6 +57,10 @@ class Estado(object):
             obj_estado.pos_coringa = pos_coringa + self.tam_x
             obj_estado.pai = self
             obj_estado.estado = ''.join(est)
+            obj_estado.profundidade = self.profundidade + 1
+
+
+            # print(str(self) + ' move abaixo')
 
         if obj_estado is not None: self.filhos.append(obj_estado)
 
@@ -62,6 +75,9 @@ class Estado(object):
             obj_estado.pos_coringa = pos_coringa - self.tam_x
             obj_estado.pai = self
             obj_estado.estado = ''.join(est)
+            obj_estado.profundidade = self.profundidade + 1
+
+            # print(str(self) + ' move acima')
 
         if obj_estado is not None: self.filhos.append(obj_estado)
 
@@ -76,6 +92,9 @@ class Estado(object):
             obj_estado.pos_coringa = pos_coringa - 1
             obj_estado.pai = self
             obj_estado.estado = ''.join(est)
+            obj_estado.profundidade = self.profundidade + 1
+
+            # print(str(self) + ' move esquerda')
 
         if obj_estado is not None: self.filhos.append(obj_estado)
 
@@ -90,17 +109,18 @@ class Estado(object):
             obj_estado.pos_coringa = pos_coringa + 1
             obj_estado.pai = self
             obj_estado.estado = ''.join(est)
+            obj_estado.profundidade = self.profundidade + 1
+
+            # print(str(self) + ' move direita')
 
         if obj_estado is not None: self.filhos.append(obj_estado)
 
     def __str__(self):
         ret = ''
-        cont = 0
         for i in range(len(self.estado)):
             if i % self.tam_x == 0:
                 ret += "\n"
             ret += " " + self.estado[i]
-
         return ret
 
     def __eq__(self, other):
@@ -112,20 +132,54 @@ class Estado(object):
     def __ne__(self, other):
         """Define a non-equality test"""
         if isinstance(other, self.__class__):
-            return not self.estado.__eq__(other.estado)
+            return not self.__eq__(other)
         return NotImplemented
 
 
+def gerar_estado_aleatorio(str_estado):
+    import random
+    l = list(str_estado)
+    random.shuffle(l)
+    return ''.join(l)
+
 if __name__ == '__main__':
+    str_estado_inicial = "AXBCDEFGH"
+    # str_estado_inicial = gerar_estado_aleatorio("ABXCDEFGH")
+
+
     estado_objetivo = Estado("XABCDEFGH")
-    estado = Estado("ABCDEXFGH", (3, 3))
-    cont = 0
-    while estado is not None:
-        print(estado)
-        if estado == estado_objetivo: break
-        estado = estado.proximo()
-        cont += 1
-        print(cont)
+    estado = Estado(str_estado_inicial, (3, 3))
+
+    print("-----------------------------------------")
+    print("Estado Inicial" + str(estado))
+    print("-----------------------------------------")
+
+    while estado != estado_objetivo:
+        try:
+            estado = estado.proximo()
+            print("-----------------------------------------")
+            print("P : " + str(estado.profundidade))
+            print("ID: " + str(estado.id))
+            print(estado)
+            print("-----------------------------------------")
+        except:
+            print("Nao foi possivel encontrar nesse espaco de busca")
+            break
+
+
+
+        # if estado.profundidade in caminho:
+        #     print("acho")
+        # caminho.append(estado.profundidade)
+
+
+
+        # # DEBUG
+        # if estado in caminho: print(estado)
+        # if estado.estado[0] == "X":
+        #     caminho.append(estado)
+
+
 
 
 
